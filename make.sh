50d34dea8eb628f3b23a8f3b2b852d9559ae4322
Andrew Pillar <me@andrewpillar.com>
2020-07-24 11:51:58 +0100

    Resolve tag via git describe

--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-BUILD="-X=main.build=$(git rev-parse HEAD) -X=main.version=$(git tag | head -n 1)"
+BUILD="-X=main.build=$(git rev-parse HEAD) -X=main.version=$(git describe --tags --abbrev=0)"
 
 set -x
 go build -tags "netgo" -ldflags "$BUILD" -o mdsrv.out
