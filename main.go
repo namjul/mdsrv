81ced7fcbc41e4583a2ff39ff53518132dd1b749
Andrew Pillar <me@andrewpillar.com>
2020-07-24 11:15:23 +0100

    Protect against path traversal, add logging and graceful shutdown

--- main.go
+++ main.go
@@ -2,26 +2,27 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"flag"
 	"fmt"
-	"text/template"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"path/filepath"
 	"strconv"
 	"strings"
+	"text/template"
+	"time"
 
 	"github.com/russross/blackfriday"
 )
 
-type server struct {
-	addr string
-	dir  string
-	tmpl *template.Template
-	cert string
-	key  string
-}
+var (
+	version string
+	build   string
+)
 
 func html(w http.ResponseWriter, content string, status int) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
@@ -37,67 +38,91 @@ func text(w http.ResponseWriter, content string, status int) {
 	w.Write([]byte(content))
 }
 
-func (s server) handle(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/" {
-		r.URL.Path = "readme"
-	}
+func documentHandler(log *log.Logger, dir string, t *template.Template) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r.URL.Path = strings.Replace(r.URL.Path, "..", "", -1)
 
-	if !strings.HasSuffix(r.URL.Path, ".md") {
-		r.URL.Path += ".md"
-	}
+		if r.URL.Path == "/" {
+			r.URL.Path = "readme"
+		}
 
-	b, err := ioutil.ReadFile(filepath.Join(s.dir, r.URL.Path))
+		if !strings.HasSuffix(r.URL.Path, ".md") {
+			r.URL.Path += ".md"
+		}
 
-	if err != nil {
-		if os.IsNotExist(err) {
-			text(w, "Not Found", http.StatusNotFound)
+		info, err := os.Stat(filepath.Join(dir, r.URL.Path))
+
+		if err != nil {
+			if os.IsNotExist(err) {
+				text(w, "Not Found", http.StatusNotFound)
+				return
+			}
+			log.Println("ERROR", err)
+			text(w, "Something went wrong", http.StatusInternalServerError)
 			return
 		}
 
-		fmt.Fprintf(os.Stderr, "%s\n", err)
+		if info.IsDir() {
+			r.URL.Path = r.URL.Path + "/readme.md"
+		}
 
-		text(w, "Something went wrong", http.StatusInternalServerError)
-		return
-	}
+		b, err := ioutil.ReadFile(filepath.Join(dir, r.URL.Path))
 
-	if strings.HasPrefix(r.Header.Get("Content-Type"), "text/plain") || s.tmpl == nil {
-		text(w, string(b), http.StatusOK)
-		return
-	}
+		if err != nil {
+			if os.IsNotExist(err) {
+				text(w, "Not Found", http.StatusNotFound)
+				return
+			}
+			log.Println("ERROR", err)
+			text(w, "Something went wrong", http.StatusInternalServerError)
+			return
+		}
 
-	buf := &bytes.Buffer{}
+		if strings.HasPrefix(r.Header.Get("Accept"), "text/plain") || t == nil {
+			text(w, string(b), http.StatusOK)
+			return
+		}
 
-	md := blackfriday.Run(b, blackfriday.WithExtensions(blackfriday.CommonExtensions))
+		buf := &bytes.Buffer{}
 
-	data := struct {
-		Document string
-	}{
-		Document: string(md),
-	}
+		md := blackfriday.Run(b, blackfriday.WithExtensions(blackfriday.CommonExtensions))
 
-	if err := s.tmpl.Execute(buf, data); err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
+		name := strings.TrimSuffix(info.Name(), ".md")
+		title := strings.Title(strings.Replace(name, "-", " ", -1))
 
-		text(w, "Something went wrong", http.StatusInternalServerError)
-		return
-	}
-	html(w, buf.String(), http.StatusOK)
+		data := struct {
+			Title    string
+			Document string
+		}{
+			Title:    title,
+			Document: string(md),
+		}
+
+		if err := t.Execute(buf, data); err != nil {
+			log.Println("ERROR", err)
+			text(w, "Something went wrong", http.StatusInternalServerError)
+			return
+		}
+		html(w, buf.String(), http.StatusOK)
+	})
 }
 
-func (s server) serve() error {
-	if s.cert != "" && s.key != "" {
-		return http.ListenAndServeTLS(s.addr, s.cert, s.key, http.HandlerFunc(s.handle))
+func serve(srv *http.Server, cert, key string) error {
+	if cert != "" && key != "" {
+		return srv.ListenAndServeTLS(cert, key)
 	}
-	return http.ListenAndServe(s.addr, http.HandlerFunc(s.handle))
+	return srv.ListenAndServe()
 }
 
 func main() {
 	var (
-		addr string
-		dir  string
-		tmpl string
-		cert string
-		key  string
+		addr        string
+		dir         string
+		tmpl        string
+		cert        string
+		key         string
+		logname     string
+		showversion bool
 	)
 
 	flag.StringVar(&addr, "addr", ":8080", "the address to serve on")
@@ -105,14 +130,16 @@ func main() {
 	flag.StringVar(&tmpl, "tmpl", "", "the template file for the documents")
 	flag.StringVar(&cert, "cert", "", "the server certificate to use for TLS")
 	flag.StringVar(&key, "key", "", "the server key to use for TLS")
+	flag.StringVar(&logname, "log", "/dev/stdout", "the file to log errors to")
+	flag.BoolVar(&showversion, "version", false, "show the version")
 	flag.Parse()
 
-	srv := server{
-		addr: addr,
-		dir:  dir,
-		cert: cert,
-		key:  key,
+	if showversion {
+		fmt.Println(version, build)
+		return
 	}
+
+	var t *template.Template
 
 	if tmpl != "" {
 		b, err := ioutil.ReadFile(tmpl)
@@ -122,13 +149,12 @@ func main() {
 			os.Exit(1)
 		}
 
-		t, err := template.New(tmpl).Parse(string(b))
+		t, err = template.New(tmpl).Parse(string(b))
 
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%s: failed to parse template: %s\n", os.Args[0], err)
 			os.Exit(1)
 		}
-		srv.tmpl = t
 	}
 
 	info, err := os.Stat(dir)
@@ -143,8 +169,47 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := srv.serve(); err != nil {
-		fmt.Fprintf(os.Stderr, "%s: %s\n", os.Args[0], err)
-		os.Exit(1)
+	log := log.New(os.Stdout, "", log.Ldate|log.Ltime|log.LUTC)
+
+	if logname != "/dev/stdout" {
+		f, err := os.OpenFile(logname, os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.FileMode(0644))
+
+		if err != nil {
+			log.Println("ERROR", "failed to open log file", err, "using stdout")
+		} else {
+			defer f.Close()
+
+			log.SetOutput(f)
+		}
 	}
+
+	srv := &http.Server{
+		Addr:         addr,
+		Handler:      documentHandler(log, dir, t),
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	go func() {
+		if err := serve(srv, cert, key); err != nil {
+			if err != http.ErrServerClosed {
+				log.Println("ERROR", err)
+			}
+		}
+	}()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	log.Println("INFO  ", "serving markdown documents in", dir, "on", addr)
+
+	c := make(chan os.Signal, 1)
+
+	signal.Notify(c, os.Interrupt)
+
+	sig := <-c
+
+	srv.Shutdown(ctx)
+
+	log.Println("INFO  ", "received signal", sig, "shutting down")
 }
